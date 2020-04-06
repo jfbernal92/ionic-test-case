829bce7ebaf056a55841fc8804bd53ded304d417
Juan Francisco Bernal <jota.efebr@hotmail.com>
2020-04-06 20:11:40 +0200

    * Created the Api Service * Deleted unused tab * Added paths to environment

--- src/app/tabs/tabs.module.ts
+++ src/app/tabs/tabs.module.ts
@@ -1,3 +1,4 @@
+import { ApiService } from './../services/api.service';
 import { IonicModule } from '@ionic/angular';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
@@ -14,6 +15,7 @@ import { TabsPage } from './tabs.page';
     FormsModule,
     TabsPageRoutingModule
   ],
-  declarations: [TabsPage]
+  declarations: [TabsPage],
+  providers: [ApiService]
 })
 export class TabsPageModule {}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiHost: 'http://hapi.fhir.org/baseR4/',
+  patientPath: 'Patient',
+  medicationRequestPath: 'MedicationRequest'
 };
 
 /*
